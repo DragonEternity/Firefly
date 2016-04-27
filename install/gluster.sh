bfd51710cae4a20a23962d49e755ea5dde86c7de
Eternity <remcostoelwinder@hotmail.com>
2016-04-27 20:45:46 +0200

    Woops

--- install/gluster.sh
+++ install/gluster.sh
@@ -1,4 +1,4 @@
 wget -O - http://download.gluster.org/pub/gluster/glusterfs/LATEST/rsa.pub | apt-key add -
-deb http://download.gluster.org/pub/gluster/glusterfs/LATEST/Debian/jessie/apt jessie main > /etc/apt/sources.list.d/gluster.list
+echo deb http://download.gluster.org/pub/gluster/glusterfs/LATEST/Debian/jessie/apt jessie main > /etc/apt/sources.list.d/gluster.list
 apt-get update
 apt-get --yes --force-yes install glusterfs-server glusterfs-client
--- install/saltminion.sh
+++ install/saltminion.sh
@@ -1,4 +1,4 @@
 wget -q -O- "http://debian.saltstack.com/debian-salt-team-joehealy.gpg.key" | apt-key add -
-deb http://debian.saltstack.com/debian jessie-saltstack main > /etc/apt/sources.list.d/saltstack.list
+echo deb http://debian.saltstack.com/debian jessie-saltstack main > /etc/apt/sources.list.d/saltstack.list
 apt-get update
 apt-get --yes --force-yes install salt-minion
