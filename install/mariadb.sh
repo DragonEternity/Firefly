2365ebce40ad1fbb4263445ae76676702eb689b5
Eternity <remcostoelwinder@hotmail.com>
2016-04-29 23:51:14 +0200

    Why do I keep forgetting these please make it stop.

--- install/mariadb.sh
+++ install/mariadb.sh
@@ -1,4 +1,4 @@
-apt-get install software-properties-common
+apt-get --yes --force-yes install software-properties-common
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
 add-apt-repository 'deb [arch=amd64,i386] http://ftp.nluug.nl/db/mariadb/repo/10.1/debian jessie main'
 apt-get update
--- install/zfs.sh
+++ install/zfs.sh
@@ -1,4 +1,4 @@
-apt-get install lsb-release
+apt-get --yes --force-yes install lsb-release
 cd /tmp && wget http://archive.zfsonlinux.org/debian/pool/main/z/zfsonlinux/zfsonlinux_6_all.deb
 dpkg -i zfsonlinux_6_all.deb
 apt-get update
