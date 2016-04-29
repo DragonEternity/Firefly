dd99c17dd4f660c58a6fbfebb210eefeb2c512e6
Eternity <remcostoelwinder@hotmail.com>
2016-04-29 23:47:21 +0200

    Forgot 2 force yes.

--- install/zfs.sh
+++ install/zfs.sh
@@ -2,4 +2,4 @@ apt-get install lsb-release
 cd /tmp && wget http://archive.zfsonlinux.org/debian/pool/main/z/zfsonlinux/zfsonlinux_6_all.deb
 dpkg -i zfsonlinux_6_all.deb
 apt-get update
-apt-get install debian-zfs
+apt-get --yes --force-yes install debian-zfs
