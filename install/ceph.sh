2cb880a55ffbe0faf47278f497dc00401656ce1f
Eternity <remcostoelwinder@hotmail.com>
2016-05-12 15:47:48 +0200

    Ceph script.

---
+++ install/ceph.sh
@@ -0,0 +1,4 @@
+wget -q -O- 'https://download.ceph.com/keys/release.asc' | apt-key add -
+echo deb http://download.ceph.com/debian-{ceph-stable-release}/ $(lsb_release -sc) main | tee /etc/apt/sources.list.d/ceph.list
+apt-get update
+apt-get install --force --force-yes ceph-deploy
