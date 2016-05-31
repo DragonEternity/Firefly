61b9b82b51d1935b06089323d3be9a33afed3397
Eternity <remcostoelwinder@hotmail.com>
2016-05-31 19:42:43 +0200

    Oops

--- install/mongo.sh
+++ install/mongo.sh
@@ -1,4 +1,4 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.2 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 apt-get update
-apt-get install -yes --force-yes mongodb-org
+apt-get install --yes --force-yes mongodb-org
