c60777039416d143836b5fabb37c4bb253301a60
Eternity <remcostoelwinder@hotmail.com>
2016-09-11 18:56:00 +0200

    Added Ansible

---
+++ install/saltstack.sh
@@ -0,0 +1,4 @@
+wget -q -O- "http://debian.saltstack.com/debian-salt-team-joehealy.gpg.key" | apt-key add -
+echo deb http://debian.saltstack.com/debian jessie-saltstack main > /etc/apt/sources.list.d/saltstack.list
+apt-get update
+apt-get --yes --force-yes install salt-minion
