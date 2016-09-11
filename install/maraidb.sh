25c08f1cf172c275ec56e85f46a43cd5a40158a1
Eternity <remcostoelwinder@hotmail.com>
2016-09-11 19:01:45 +0200

    Oops

--- install/maraidb.sh
+++
@@ -1,5 +0,0 @@
-apt-get install software-properties-common
-apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
-add-apt-repository 'deb [arch=amd64,i386] http://ftp.nluug.nl/db/mariadb/repo/10.1/debian jessie main'
-apt-get update
-apt-get install mariadb-server
