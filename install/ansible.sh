43214a0e77a06a3e87ab555015c3bff16d77eeec
DragonEternity <remcostoelwinder@hotmail.com>
2016-09-12 09:40:09 +0200

    Update ansible.sh
    Pfft

--- install/ansible.sh
+++ install/ansible.sh
@@ -1,4 +1,4 @@
 apt-get install software-properties-common
 apt-add-repository ppa:ansible/ansible
 apt-get update
-apt-get --yes --forece-yes install ansible
+apt-get --yes --force-yes install ansible
