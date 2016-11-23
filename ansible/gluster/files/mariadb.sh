3ea6684a97bdba809e545c4b378156b49acb590c
DragonEternity <remcostoelwinder@hotmail.com>
2016-11-23 11:09:57 +0100

    Update mariadb.sh

--- ansible/gluster/files/mariadb.sh
+++ ansible/gluster/files/mariadb.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 apt-get --yes --force-yes install software-properties-common
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
 add-apt-repository 'deb [arch=amd64,i386] http://ftp.nluug.nl/db/mariadb/repo/10.1/debian jessie main'
