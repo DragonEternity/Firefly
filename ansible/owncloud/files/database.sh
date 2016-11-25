a8d9473d341a85dd812c8199aaec28378e4c3ed6
DragonEternity <remcostoelwinder@hotmail.com>
2016-11-25 16:03:56 +0100

    Update database.sh

--- ansible/owncloud/files/database.sh
+++ ansible/owncloud/files/database.sh
@@ -1,3 +1,2 @@
 mysql -u root -pKOjgkn39g8sdv -e "CREATE DATABASE owncloud;"
-mysql -u root -pKOjgkn39g8sdv -e "GRANT ALL ON owncloud.* to 'owncloud'@'localhost' IDENTIFIED BY '1234'"
 mysql -u root -pKOjgkn39g8sdv -e "RENAME USER 'root'@'localhost' TO 'boat'@'localhost'"
