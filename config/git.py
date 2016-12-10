529daf854198cb459f55073a8ac3381dd879b402
user <user@perf>
2016-12-10 17:19:08 +0100

    Added git script in python

---
+++ config/git.py
@@ -0,0 +1,12 @@
+# Python script for comparing git dirs. It's basically bash but with easier vars.
+from subprocess import call
+dir = "/home/user/Testing" 
+
+def compare():
+	call(["git", "-C", dir, "fetch"])
+	LOCAL = call(["git", "-C", dir, "rev-parse", "@"])
+	BASE = call(["git", "-C", dir, "merge-base", "@", "master"])
+	if LOCAL == BASE:
+		call(["git", "-C", dir, "pull"])
+compare()
+
