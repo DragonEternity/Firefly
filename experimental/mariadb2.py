8535c21fae68125bff8004a84195ab61bfcbff2a
Remco Stoelwinder <remcostoelwinder@hotmail.com>
2016-12-12 21:02:14 +0100

    Update mariadb2.py

--- experimental/mariadb2.py
+++ experimental/mariadb2.py
@@ -8,12 +8,12 @@ repo = "deb [arch=amd64,i386] http://ftp.nluug.nl/db/mariadb/repo/10.1/debian je
 pw = "lol"
 
 def preinstall():
-     call(["apt-get", "install", "--yes", "--force-yes", "software-properties-common"], stdout=open(os.devnull))
-     call(["apt-key", "adv", "--recv-keys", "--keyserver", "keyserver.ubuntu.com", key], stdout=open(os.devnull))
-     call(["add-apt-repository", repo], stdout=open(os.devnull))
-     call(["apt-get", "--yes", "--force-yes", "update"], stdout=open(os.devnull)) 
+     call(["apt-get", "install", "--yes", "--force-yes", "software-properties-common"])
+     call(["apt-key", "adv", "--recv-keys", "--keyserver", "keyserver.ubuntu.com", key])
+     call(["add-apt-repository", repo])
+     call(["apt-get", "--yes", "--force-yes", "update"]) 
      os.environ["DEBIAN_FRONTEND"] = "noninteractive"
-     call(["apt-get", "install", "--yes", "--force-yes", "mariadb-server-10.1"], stdout=open(os.devnull)) 
+     call(["apt-get", "install", "--yes", "--force-yes", "mariadb-server-10.1"]) 
 
 def changepwd():
      call(["mysql", "-u", "root", "-e", "SET PASSWORD FOR root@localhost = PASSWORD("+"'"+pw+"'"+");"])
