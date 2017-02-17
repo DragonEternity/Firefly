a0892574742425d406be21ecbb4d41a1ceb26872
Vagrant Default User <vagrant@Ans>
2017-02-17 11:38:35 +0000

    Added something fucky

---
+++ experimental/owncloud/roles/owncloud1/files/config.php
@@ -0,0 +1,30 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc31076ee98e',
+  'passwordsalt' => '2282b318c53567728bef3cfeb4e823',
+  'secret' => '92692cb417b42d297373cdaff7f18a3a61c51283709c803f040df28891047cc3ed3cf038679af3882b736be7efff31a9',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.178.48',
+  ),
+  'datadirectory' => '/usr/share/owncloud/data',
+  'overwrite.cli.url' => 'http://192.168.178.48/owncloud',
+  'dbtype' => 'mysql',
+  'version' => '7.0.4.2',
+  'appstoreenabled' => false,
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/usr/share/owncloud/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+  ),
+  'dbname' => 'owncloud',
+  'dbhost' => '192.168.178.45',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'oc_admin',
+  'dbpassword' => 'dba096889cfeeb0951ac0ba35052cd',
+  'installed' => true,
+);
