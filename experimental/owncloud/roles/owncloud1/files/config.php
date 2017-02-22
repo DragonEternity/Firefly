f86231930dfc9fb346a95ac011c58e9c22e60f3d
Vagrant Default User <vagrant@Ans>
2017-02-22 13:59:32 +0000

    Lol

--- experimental/owncloud/roles/owncloud1/files/config.php
+++ experimental/owncloud/roles/owncloud1/files/config.php
@@ -1,22 +1,23 @@
 <?php
 $CONFIG = array (
   'updatechecker' => false,
-  'instanceid' => 'occum0ivd65t',
-  'passwordsalt' => 'o2jo8zRUCC5eQL2kobNu9ai4UCIc4Q',
-  'secret' => 'DHS5krqvMmqMHLHhcQfWAuW9OLxUxztxXQTBuhezsS6fgoLl',
+  'instanceid' => 'ocea6g33ffzk',
+  'passwordsalt' => 'FDDg1vWb7+f/GaIUFx9mR/ivnCzaRM',
+  'secret' => 'PpCzZCs+EwgENmHpx/gMAIqUzW/XPEP80WNLyTagzldv8Ac2',
   'trusted_domains' => 
   array (
-    0 => '192.168.178.54',
+    0 => '192.168.178.74',
   ),
   'datadirectory' => '/var/www/owncloud/data',
-  'overwrite.cli.url' => 'http://192.168.178.54/owncloud',
+  'overwrite.cli.url' => 'http://192.168.178.74/owncloud',
   'dbtype' => 'mysql',
   'version' => '9.1.4.2',
   'dbname' => 'owncloud',
-  'dbhost' => '192.168.178.53',
+  'dbhost' => '192.168.178.70',
   'dbtableprefix' => 'oc_',
-  'dbuser' => 'oc_admin1',
-  'dbpassword' => 'k88iidEKkfrmBNwBhxbDC8AmchSGxO',
+  'dbuser' => 'oc_admin2',
+  'dbpassword' => '53bsgvHvZBIL+TAHvlkDD78FX1HOj5',
   'logtimezone' => 'UTC',
   'installed' => true,
 );
+
